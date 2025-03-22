393942e2a5c71a0aab52b68755570a88e754ed21
Pavel1523 <mrkva.650227@gmail.com>
2025-03-22 13:31:53 +0100

    verze_2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # projekt_3.py: třetí projekt do Engeto Online Python Akademie
+# Verze č.2
 # autor : Ing. Pavel Mrkva
 # email: pavel.mrkva@seznam.cz
 
-
 import sys
 import csv
+import time
 import requests as r
 from bs4 import BeautifulSoup as bs
 import logging
@@ -18,15 +19,13 @@ def nastav_logovani() -> None:
     )
 
 
-def kontrola_dostupnosti_serveru(odkaz: str) -> bool:
-    """Kontroluje dostupnost URL."""
-    try:
-        odpoved = r.get(odkaz, timeout=10)
-        if odpoved.status_code == 200:
-            return True
-    except r.RequestException as e:
-        logging.error(f"Chyba při připojení k URL {odkaz}: {e}")
-    return False
+def inicializace() -> tuple[str, str]:
+    """Ověřuje správnost vstupních argumentů."""
+    R = "\033[31m"  # Červená barva
+    RE = "\033[0m"  # Reset na výchozí barvu
+    if not kontrola_na_vstupu(sys.argv):
+        raise SystemExit(f"{R}Neplatné vstupní argumenty!\n{RE}" + 90 * "=")
+    return sys.argv[1], sys.argv[2]
 
 
 def kontrola_na_vstupu(sys_argv: list[str]) -> bool:
@@ -62,20 +61,85 @@ def kontrola_na_vstupu(sys_argv: list[str]) -> bool:
     return True  # je True, pokud je všechno ve funkci True a umožní spuštění skriptu
 
 
-def kontrola_reakce_serveru(odkaz: str) -> r.Response | None:
-    """Kontrola prodlevy reakce serveru."""
+def kontrola_dostupnosti_serveru(odkaz: str) -> bool:
+    """Kontroluje dostupnost URL."""
     try:
-        return r.get(
-            odkaz, timeout=10
-        )  # do 10 sec. od požadavku musí server reagovat a vratit data
+        odpoved = r.get(odkaz, timeout=10)
+        if odpoved.status_code == 200:
+            return True
     except r.RequestException as e:
-        logging.error(f"Chyba při získávání dat z '{odkaz}': {e}")
-        return None
+        logging.error(f"Chyba při připojení k URL {odkaz}: {e}")
+    return False
+
+
+def kontrola_reakce_serveru(odkaz: str) -> r.Response | None:
+    """Kontrola prodlevy reakce serveru s opakováním."""
+    for attempt in range(3):  # máme 3 pokusy k navázání spojení se serverem
+        try:
+            odpoved = r.get(odkaz, timeout=10)
+            if odpoved.status_code == 200:
+                return odpoved
+            logging.warning(
+                f"Neúspěšný pokus {attempt + 1}/3: Server vrátil {odpoved.status_code}"
+            )
+        except r.RequestException as e:
+            logging.error(
+                f"Chyba při získávání dat ({attempt + 1}/3) z '{odkaz}': {e}"
+            )
+        time.sleep(2)  # Pauza mezi pokusy
+    return None
 
 
 def generovani_soup(res: r.Response | None) -> bs | None:
     """Převod html do objektu beautifulsoup."""
     return bs(res.text, "html.parser") if res else None
+
+
+def ziskani_seznamu_obci(odkaz_stranky: str) -> list:
+    """Stahuje HTML se seznamem obcí a převede jej na 'soup'."""
+    R = "\033[31m"  # Červená barva
+    RE = "\033[0m"  # Reset na výchozí barvu
+    odpoved = kontrola_reakce_serveru(odkaz_stranky)
+    if not odpoved:
+        raise SystemExit(
+            f"{R}Chyba: nelze získat odpověď ze serveru {odkaz_stranky}\n{RE}"
+            + "=" * 90
+        )
+    soup = generovani_soup(odpoved)
+    return filtr_vsechny_tagy(soup, "table")
+
+
+def zpracuj_obec(row) -> dict[str, str]:
+    """Extrahuje kód obce, název obce a odkaz na detailní stránku výsledků a volá 'def scrapuj_obec'"""
+    td_tags = row.find_all("td", ["cislo", "overflow_name"])
+    if not td_tags or not td_tags[0].a:
+        return {}
+    obec_odkaz = generovani_odkazu_k_obci(
+        "https://volby.cz/pls/ps2017nss/", td_tags[0].a.get("href")
+    )
+    return scrapuj_obec(obec_odkaz, td_tags)
+
+
+def scrapuj_obec(obec_odkaz: str, td_tags) -> dict[str, str]:
+    """Používá zadaný odkaz na detailní stránku obce, stáhne ji, extrahuje z ní počet voličů, platné hlasy a výsledky politických stran, a vrátí vše jako slovník."""
+    odpoved = kontrola_reakce_serveru(obec_odkaz)
+    soup = generovani_soup(odpoved)
+    tables = filtr_vsechny_tagy(soup, "table")
+    strany = zpracuj_strany(tables[1:])
+    return generovani_vysledku(
+        td_tags[0].text,  # kód obce
+        td_tags[1].text,  # název obce
+        hledani_tagu_dle_volby(
+            tables[0], "td", {"headers": "sa2"}
+        ),  # voliči v seznamu
+        hledani_tagu_dle_volby(
+            tables[0], "td", {"headers": "sa3"}
+        ),  # vydané obálky
+        hledani_tagu_dle_volby(
+            tables[0], "td", {"headers": "sa6"}
+        ),  # platné hlasy
+        strany,
+    )
 
 
 def filtr_vsechny_tagy(tag: bs | None, typ: str) -> list:
@@ -95,11 +159,24 @@ def generovani_odkazu_k_obci(base_url: str, cast_odkazu: str) -> str:
     return f"{base_url}{cast_odkazu}"
 
 
-def cistit_cislo(text: str) -> str:
-    """Odstraní z textu nevhodnou část (html-mezera -> znak na výstupu)."""
-    return (
-        text.replace("\xa0", "").replace(" ", "") if text else ""
-    )  # odstraní nevhodnou část textu vzniklou zadáním mezery v html
+def zpracuj_strany(tables) -> dict[str, str]:
+    """Získá volební výsledky strany a vrátí je jako slovník jméno:výsledky."""
+    strany = {}  # slovník  {"strana" : "počet hlasů"}
+    for table in tables:  # procházím tabulky na url pro každou obec
+        for row in filtr_vsechny_tagy(
+            table, "tr"
+        ):  # procházím všechny řádky tabulek
+            column = filtr_vsechny_tagy(
+                row, "td"
+            )  # získávám sloupce v jednotlivých tabulkách na url obcí
+            if (
+                column and len(column) > 2 and column[1].text != "-"
+            ):  # vybírám pouze tabulky se 3-mi neprázdnými sloupci, kde jsou údaje o počtu hlasů, které strany dostaly
+
+                strany[column[1].text] = column[
+                    2
+                ].text  # název strany, počet hlasů
+    return strany
 
 
 def generovani_vysledku(
@@ -127,122 +204,58 @@ def generovani_vysledku(
     }
 
 
+def cistit_cislo(text: str) -> str:
+    """Odstraní z textu nevhodnou část (html-mezera -> znak na výstupu)."""
+    return (
+        text.replace("\xa0", "").replace(" ", "") if text else ""
+    )  # odstraní nevhodnou část textu vzniklou zadáním mezery v html
+
+
 def zapis_do_csv(data: list[dict[str, str]], jmeno_souboru: str) -> None:
     """Zapisuje výsledky scrapování do soubou *.csv."""
     try:
+        G = "\033[032m"  # green
+        RE = "\033[0m"  # reset na výchozí barvu
         with open(
             jmeno_souboru, mode="w", encoding="utf-8", newline=""
         ) as csv_soubor:
             zapis = csv.DictWriter(csv_soubor, fieldnames=data[0].keys())
             zapis.writeheader()
             zapis.writerows(data)
-        logging.info(f"Data se zapsala do souboru: '{jmeno_souboru}'.")
+        logging.info(f"Data se zapsala do souboru: {G}'{jmeno_souboru}'{RE}.")
     except Exception as e:
         logging.error(f"Chyba při zápisu do souboru: {e}!")
 
 
-def scraping() -> None:
-    """Procedura scrapování."""
-    nastav_logovani()  # počátek loggování
-
-    if not kontrola_na_vstupu(sys.argv):  # kontrola vstupních argumentů
-        sys.exit(1)
-    odkaz_stranky: str = sys.argv[1]  # vstupní parametr č. 1
-    nazev_souboru: str = sys.argv[2]  # vstupní parametr č. 2
-    seznam: list[dict[str, str]] = (
-        []
-    )  # list, do kterého se postupně zapisují slovníky s daty
-    odpoved_1: r.Response | None = kontrola_reakce_serveru(
-        odkaz_stranky
-    )  # další kontrola serveru před zahájením scrapování včetně získání html
-    if not odpoved_1:
-        sys.exit(1)
-    logging.info("Úvodní sekvence je v pořádku, scraping je zahájen!")
-
-    soup_1: bs | None = generovani_soup(
-        odpoved_1
-    )  # převedení html úvodní url na objekt bs
-    tables: list = filtr_vsechny_tagy(
-        soup_1, "table"
-    )  # selekce tagů na úvodní url s atributem "table", které obsahují požadované údaje (kód obce, název obce, link na obec)
-
+def main() -> None:
+    """Hlavní funkce."""
+    R = "\033[31m"  # Červená barva
+    G = "\033[032m"  # green
+    RE = "\033[0m"  # Reset na výchozí barvu
+    nastav_logovani()
+    print(90 * "=")
+    odkaz_stranky, nazev_souboru = inicializace()
+    seznam = []
+    tables = ziskani_seznamu_obci(odkaz_stranky)
+    logging.info("Prvotní vstupní údaje prověřeny.")
+    logging.info("Proces scrapování dat zahájen.")
     logging.info("Stahují se data ze serveru, čekej ...")
 
-    for table in tables:  # procházíme všechny tabulky úvodního url
-
-        for row in filtr_vsechny_tagy(
-            table, "tr"
-        ):  # výběr řádků postupně všech tabulek v úvodní url
-            td_tags = row.find_all(
-                "td", ["cislo", "overflow_name"]
-            )  # získání dat ve sloupcích všech tabulek v uvodní url
-            if td_tags:
-                obec_odkaz: str = generovani_odkazu_k_obci(
-                    "https://volby.cz/pls/ps2017nss/", td_tags[0].a.get("href")
-                )  # url na jednotlivé obce (ukryto v atributu "a" ve sloupci  class="číslo")
-                odpoved_2 = kontrola_reakce_serveru(
-                    obec_odkaz
-                )  # kontrola reakce serveru na url postupně každé obce
-                soup_2 = generovani_soup(
-                    odpoved_2
-                )  # převedení html každé jednotlivé obce  na objekt bs
-                tables = filtr_vsechny_tagy(
-                    soup_2, "table"
-                )  # selekce tagů na url obce s atributem "table", které obsahují požadované údaje (počet voličů, vydané obálky.....)
-
-                strany: dict[str, str] = (
-                    {}
-                )  # slovník  {"strana" : "počet hlasů"}
-
-                for table in tables[
-                    1:
-                ]:  # procházím tabulky na url pro každou obec
-                    for row in filtr_vsechny_tagy(
-                        table, "tr"
-                    ):  # procházím všechny řádky tabulek
-                        column = filtr_vsechny_tagy(
-                            row, "td"
-                        )  # získávám sloupce v jednotlivých tabulkách na url obcí
-                        if (
-                            column
-                            and len(column) > 2
-                            and column[1].text != "-"
-                        ):  # vybírám pouze tabulky se 3-mi neprázdnými sloupci, kde jsou údaje o počtu hlasů, které strany dostaly
-                            strany[column[1].text] = column[
-                                2
-                            ].text  # název strany, počet hlasů
-                vystup_txt: dict[str, str] = generovani_vysledku(
-                    td_tags[0].text,
-                    td_tags[1].text,
-                    hledani_tagu_dle_volby(
-                        tables[0], "td", {"headers": "sa2"}
-                    ),
-                    hledani_tagu_dle_volby(
-                        tables[0], "td", {"headers": "sa3"}
-                    ),
-                    hledani_tagu_dle_volby(
-                        tables[0], "td", {"headers": "sa6"}
-                    ),
-                    strany,
-                )
-                # získaná data postupně :
-                #   "kod obce" (získán na vstupní url v rámci běhu 2.for-cyklu),
-                #   "název obce" (získán na vstupní url v rámci běhu 2.for-cyklu),
-                #   "voliči v seznamu" (získáno na url obce rámci běhu 2.for-cyklu ve sloupci s atributem {"headers": "sa2"}),
-                #   "vydané obálky" (získáno na url obce rámci běhu 2.for-cyklu ve sloupci s atributem {"headers": "sa3"}),
-                #   "platné hlasy" (získáno na url obce rámci běhu 2.for-cyklu ve sloupci s atributem {"headers": "sa6"}),
-                #   "slovník strany" (získán na url obce v rámci běhu 3.for-cyklu)- ve funkci generovani_vysledku se ze slovníku tahají zvlášť klíče a zvlášť hodnoty
-
+    for table in tables:
+        for row in filtr_vsechny_tagy(table, "tr"):
+            vystup_txt = zpracuj_obec(row)
+            if vystup_txt:
                 seznam.append(vystup_txt)
-    if (
-        len(seznam) == 0
-    ):  # pokud je seznam prázdný (nejčastěji špatným zadáním argumentu url)
-        logging.error(f"Žádná data na výstupu, zkontroluj vstupní URL!")
-        sys.exit(1)
+    if not seznam:
+        logging.error(
+            f"{R}Nebyla získána žádná data! Zkontroluj vstupní URL!\n{RE}"
+            + 90 * "="
+        )
+        raise SystemExit(1)
     zapis_do_csv(seznam, nazev_souboru)
-
-    logging.info("Scraping je ukončen!")
+    logging.info(f"{G}Scraping je ukončen!{RE}")
+    print(90 * "=")
 
 
 if __name__ == "__main__":
-    scraping()
+    main()
